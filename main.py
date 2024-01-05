2fed7ceca8798d938d185056f98f15285454457a
Colin Benner <colin-dev@yzhs.de>
2024-01-05 19:27:34 +0100

    Implement simple first-fit-decreasing algorithm
    This should be good enough for what we need given the small numbers
    involved.  There will be situations in which we could seat more people
    by kicking out ones that already got assigned a table but we don't want
    to do that.  Detecting the first group that cannot be seated, and will
    thus end up on the waiting list, should be enough.
    
    Future work: see if there are smaller groups further down the line that
    will still fit in.

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 #!/bin/python3
 
-import z3
-
-groups = [
+group_sizes = [
     3,
     4,
     2,
@@ -20,91 +18,36 @@ groups = [
     4,
     4,
 ]
-
-o = z3.Optimize()
 
 max_group_size = 5
 max_per_table = 5
-num_tables = 12
+num_tables = 10
 
-tables = z3.IntVector("tables", num_tables)
 
-# Non-negative number of people per table
-o.add(z3.And([0 <= t for t in tables]))
+for x in range(len(group_sizes)):
+    candidate_group_sizes = sorted(group_sizes[:x+1], reverse=True)
 
-# Maximum number of people per table
-o.add(z3.And([t <= max_per_table for t in tables]))
+    groups_at_table = [[] for _ in range(num_tables)]
+    open_places_at_table = [max_per_table for _ in range(num_tables)]
 
-# Break symmetry by requiring that the number of people per table decreases
-o.add(z3.And([t1 >= t2 for t1, t2 in zip(tables, tables[1:])]))
+    for i, group_size in enumerate(candidate_group_sizes):
+        found_table_for_current_group = False
+        for j, open_places in enumerate(open_places_at_table):
+            if open_places < group_size:
+                continue
 
-# Associate groups with tables
-groups_tables = [
-    z3.IntVector(f"table_{i+1}_includes_group", len(groups)) for i in range(num_tables)
-]
+            found_table_for_current_group = True
+            groups_at_table[j].append(i)
+            open_places_at_table[j] -= group_size
+            break
 
-# Make groups_tables boolean, i.e. 0 or 1
-# 1 means the group sits at that table, 0 means it doesn't
-o.add(z3.And([0 <= x for g in groups_tables for x in g]))
-o.add(z3.And([x <= 1 for g in groups_tables for x in g]))
-
-# Each group sits at no more than one table
-for j in range(len(groups)):
-    o.add(sum([groups_tables[i][j] for i in range(num_tables)]) <= 1)
-
-# Ensure that sum of group sizes at a table does not exceed table size
-for i in range(num_tables):
-    o.add(
-        sum([groups[j] * groups_tables[i][j] for j in range(len(groups))])
-        <= max_per_table
-    )
-
-# Sync tables with groups_tables
-for i in range(num_tables):
-    o.add(
-        tables[i] == sum([groups_tables[i][j] * groups[j] for j in range(len(groups))])
-    )
-
-# Ensure that each group is seated at a table
-for j in range(len(groups)):
-    o.add(sum([groups_tables[i][j] for i in range(num_tables)]) == 1)
-
-table_of_group = z3.IntVector("table_of_group", len(groups))
-# sync with groups_tables
-for i in range(num_tables):
-    for j in range(len(groups)):
-        o.add(z3.Implies(groups_tables[i][j] == 1, table_of_group[j] == i + 1))
-
-# Break symmetry: for same number of people in group, move group with smaller
-# number to table with smaller number
-for j in range(len(groups)):
-    for k in range(j + 1, len(groups)):
-        if groups[j] != groups[k]:
-            continue
-        o.add(table_of_group[j] < table_of_group[k])
-
-# Place groups that need the whole table first
-for j, g in enumerate(groups):
-    if g == 5:
-        for k in range(j + 1, len(groups)):
-            o.add(table_of_group[j] < table_of_group[k])
-
-# Minimize number of tables used
-o.minimize(
-    sum(
-        sum([groups_tables[i][j] for j in range(len(groups))]) > 0
-        for i in range(num_tables)
-    )
-)
-
-if o.check() != z3.sat:
-    print("Unsatisfiable")
-    exit(1)
-
-model = o.model()
-print("People per table:", *[model[t] for t in tables])
-for i in range(num_tables):
-    groups_at_this_table = [
-        j for j in range(len(groups)) if model[groups_tables[i][j]] == 1
-    ]
-    print(f"Groups at table {i+1}:", *groups_at_this_table)
+        if not found_table_for_current_group:
+            print(f"There's enough room to seat the first {x-1} groups but not for the first {x}:")
+            print(*group_sizes[:x+1])
+            print(f"Putting group {x} of size {group_sizes[x]} on the waiting list")
+            print()
+            break
+    else:
+        print(f"Successfully seated groups 0 through {x}:")
+        for i in range(num_tables):
+            print("Sitting at table", i, "are groups of the following sizes:", *[candidate_group_sizes[x] for x in groups_at_table[i]])
